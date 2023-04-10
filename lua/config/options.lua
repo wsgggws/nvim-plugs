bbf4db557d9f16662febdd7a29ad3db994e6c888
wsgggws@gmail.com <Navy.Tian>
2023-04-10 22:04:06 +0800

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,12 @@
 local opt = vim.opt
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
 
-vim.opt.termguicolors = true
+opt.termguicolors = true
 
-vim.opt.showmode = false
+opt.showmode = false
