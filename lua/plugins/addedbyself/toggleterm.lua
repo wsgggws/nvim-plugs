0a0bd5a1a5dfab2b60342543330ff007ed56415d
Navy.Tian <hj.tian@aftership.com>
2023-09-21 18:00:40 +0800

    Add plugin diagflow and config it

--- lua/plugins/addedbyself/toggleterm.lua
+++ lua/plugins/addedbyself/toggleterm.lua
@@ -5,7 +5,7 @@ return {
 	keys = {
 		{ "<leader>ft", "<cmd>ToggleTerm direction=horizontal size=22<cr>", desc = "ToggleTerm bottom" },
 		{ "<leader>fT", "<cmd>ToggleTerm direction=tab<cr>", desc = "ToggleTerm tab" },
-		{ "<leader>fl", "<cmd>ToggleTerm direction=vertical size=102<cr>", desc = "ToggleTerm right" },
+		{ "<leader>fl", "<cmd>ToggleTerm direction=vertical size=92<cr>", desc = "ToggleTerm right" },
 		{ "<leader>fc", "<cmd>ToggleTermSendCurrentLine<cr>", desc = "ToggleTerm current line" },
 		{ "<leader>fs", "<cmd>ToggleTermSendVisualSelection<cr>", desc = "ToggleTerm visual selection" },
 	},
--- lua/plugins/e-nvim-lspconfig.lua
+++ lua/plugins/e-nvim-lspconfig.lua
@@ -1,6 +1,11 @@
 return {
 	"neovim/nvim-lspconfig",
 	opts = {
+		diagnostics = {
+			-- 不在当前行以虚拟文本形式显示错误信息
+			underline = false,
+			virtual_text = false,
+		},
 		setup = {
 			clangd = function(_, opts)
 				opts.capabilities.offsetEncoding = { "utf-16" }
