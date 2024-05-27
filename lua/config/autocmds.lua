9656ed74dd48f3f8baeb75759313dc4b29ceddeb
Navy.Tian <hj.tian@aftership.com>
2024-05-27 16:59:32 +0800

    update cursor ship

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,8 +16,8 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 vim.api.nvim_create_autocmd({ "ExitPre" }, {
 	callback = function()
-		-- vim.opt.guicursor = "a:ver25-blinkon250-blinkoff400-blinkwait700"
-		vim.opt.guicursor = "a:hor25-blinkon250-blinkoff400-blinkwait700"
+		vim.opt.guicursor = "a:ver25-blinkon250-blinkoff400-blinkwait700"
+		-- vim.opt.guicursor = "a:hor25-blinkon250-blinkoff400-blinkwait700"
 	end,
 })
 
